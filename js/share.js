7ffde97e656a4947f427552bad1dea87d640db06
kyum8562@gmail.com <66922359+kyum8562@users.noreply.github.com>
2021-11-29 01:23:57 +0900

    tenth commit

--- js/share.js
+++ js/share.js
@@ -19,42 +19,7 @@ function setShare(){
             webUrl : shareURL,
         },
         },
-        itemContent: {
-        profileText: 'Kakao',
-        profileImageUrl: 'http://mud-kage.kakao.co.kr/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
-        titleImageUrl: 'http://mud-kage.kakao.co.kr/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
-        titleImageText: 'Cheese cake',
-        titleImageCategory: 'Cake',
-        items: [
-            {
-            item: 'Cake1',
-            itemOp: '1000원',
-            },
-            {
-            item: 'Cake2',
-            itemOp: '2000원',
-            },
-            {
-            item: 'Cake3',
-            itemOp: '3000원',
-            },
-            {
-            item: 'Cake4',
-            itemOp: '4000원',
-            },
-            {
-            item: 'Cake5',
-            itemOp: '5000원',
-            },
-        ],
-        sum: '총 결제금액',
-        sumOp: '15000원',
-        },
-        social: {
-        likeCount: 10,
-        commentCount: 20,
-        sharedCount: 30,
-        },
+        
         buttons: [
         {
             title: '결과 확인하기',
@@ -63,7 +28,6 @@ function setShare(){
             webUrl : shareURL
             },
         },
-        
         ]
     });
 }
