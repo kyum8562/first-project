70435e5ad186008939716866d0de784c1a7ace4a
kyum8562@gmail.com <66922359+kyum8562@users.noreply.github.com>
2021-11-29 00:42:10 +0900

    seventh commit

--- js/share.js
+++ js/share.js
@@ -1,3 +1,72 @@
+const url = 'https://girin-lovetest.netlify.app/';
+
+function setShare(){
+    var resultImg = document.querySelector('#resultImg');
+    var resultAlt = resultImg.firstElementChild.lastChild;
+    const shareTitle = '연애유형 동물편 결과';
+    const shareDes = infoList[resultAlt].name;
+    const shareImage = url + 'img/image-' + resultAlt + '.png';
+    const shareURL = url + 'page/result-' + resultAlt + '.html';
+    kakaoShare = Kakao.Link.sendDefault({
+        objectType: 'feed',
+        content: {
+        title: shareTitle,
+        description: shareDes,
+        imageUrl: shareImage,
+        link: {
+            mobileWebUrl: shareURL,
+            webUrl : shareURL,
+        },
+        },
+        itemContent: {
+        profileText: 'Kakao',
+        profileImageUrl: 'http://mud-kage.kakao.co.kr/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
+        titleImageUrl: 'http://mud-kage.kakao.co.kr/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
+        titleImageText: 'Cheese cake',
+        titleImageCategory: 'Cake',
+        items: [
+            {
+            item: 'Cake1',
+            itemOp: '1000원',
+            },
+            {
+            item: 'Cake2',
+            itemOp: '2000원',
+            },
+            {
+            item: 'Cake3',
+            itemOp: '3000원',
+            },
+            {
+            item: 'Cake4',
+            itemOp: '4000원',
+            },
+            {
+            item: 'Cake5',
+            itemOp: '5000원',
+            },
+        ],
+        sum: '총 결제금액',
+        sumOp: '15000원',
+        },
+        social: {
+        likeCount: 10,
+        commentCount: 20,
+        sharedCount: 30,
+        },
+        buttons: [
+        {
+            title: '결과 확인하기',
+            link: {
+            mobileWebUrl: shareUrl,
+            webUrl : shareUrl
+            },
+        },
+        
+        ]
+    });
+}
+
 function kakaoShare(){
     Kakao.Link.sendDefault({
         objectType: 'feed',
